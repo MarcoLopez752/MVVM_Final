e94d3710805d6a980179cfdaa832fc49612cb533
Marco Lopez <marco752slayer@gmail.com>
2020-01-02 16:37:00 -0400

    save

---
+++ Trabajo/Trabajo/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trabajo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int nota { get; set; }
+    }
+}
